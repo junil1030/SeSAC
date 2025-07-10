39537b4af4afe90e602da3b83016f1ed10c1cdd0
junil1030 <dccrdseo@naver.com>
2025-07-10 21:56:57 +0900

    feat: 0710 수업 진행 과정 추가

--- Project/SeSAC7Week2/SeSAC7Week2/tamagotchi/DramaTableViewCell.swift
+++ Project/SeSAC7Week2/SeSAC7Week2/tamagotchi/DramaTableViewCell.swift
@@ -8,16 +8,8 @@
 import UIKit
 
 class DramaTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
+    
+    @IBOutlet var posterIamgeView: UIImageView!
+    @IBOutlet var overviewLabel: UILabel!
+    
 }
--- Project/SeSAC7Week2/SeSAC7Week2/tamagotchi/DramaTableViewController.swift
+++ Project/SeSAC7Week2/SeSAC7Week2/tamagotchi/DramaTableViewController.swift
@@ -8,82 +8,44 @@
 import UIKit
 
 class DramaTableViewController: UITableViewController {
+    
+    let image = ["star.fill", "star", "heart", "heart.fill", "pencil", "xmark", "person"]
 
+    var nickName: String? = "고래밥"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        // Optional >> ?, ??, ?, 옵셔널 바인딩(if let / guard)
+        
+        if let name = nickName {
+            print(name)
+        }
     }
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return image.count
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dramaCell", for: indexPath) as! DramaTableViewCell
+        
+        cell.overviewLabel.numberOfLines = 0
+        cell.overviewLabel.text = "asdasdfasdglaijczvlbk;jdsfoigjsdflkgvjcivkmjanSDOIjghyioregjvfx;kclvzjxlkcvjdsoia;gfjilxfcjvcibohjfduighw2jk3lhrqiuwehiuhxcfvkjzlhdv"
+        cell.posterIamgeView.backgroundColor = .orange
+        cell.posterIamgeView.layer.cornerRadius = 10
+        
+        let name = image[indexPath.row]
+        cell.posterIamgeView.image = UIImage(systemName: name)
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
