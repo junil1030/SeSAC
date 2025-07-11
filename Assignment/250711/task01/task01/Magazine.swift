685f6e50fc90f5628ea9225b681b4c66408c0ea4
junil1030 <dccrdseo@naver.com>
2025-07-11 23:14:38 +0900

    feat: Magazine Model 생 성 및 Dummy Data 추가

---
+++ Assignment/250711/task01/task01/Magazine.swift
@@ -0,0 +1,16 @@
+//
+//  Magazine.swift
+//  task01
+//
+//  Created by 서준일 on 7/11/25.
+//
+
+import Foundation
+
+struct Magazine {
+    let title: String
+    let subtitle: String
+    let photo_image: String
+    let date: String
+    let link: String
+}
