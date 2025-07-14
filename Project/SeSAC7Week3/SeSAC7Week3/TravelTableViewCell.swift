a9100c17361276ec58e0fb307471f3b4fa1eaa90
junil1030 <dccrdseo@naver.com>
2025-07-14 15:20:15 +0900

    refactor: 화면 조정

---
+++ Project/SeSAC7Week3/SeSAC7Week3/TravelTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelTableViewCell.swift
+//  SeSAC7Week3
+//
+//  Created by 서준일 on 7/14/25.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Project/SeSAC7Week3/SeSAC7Week3/UIViewController+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewController+Extension.swift
+//  SeSAC7Week3
+//
+//  Created by 서준일 on 7/14/25.
+//
+
+import Foundation
