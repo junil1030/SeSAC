2efe74583feacfccbfaf45e71e5493c0e2975ac7
junil1030 <dccrdseo@naver.com>
2025-07-14 15:23:04 +0900

    feat: TravelCell에 대한 클래스 생성

--- Project/SeSAC7Week3/SeSAC7Week3/TravelTableViewCell.swift
+++ Project/SeSAC7Week3/SeSAC7Week3/TravelTableViewCell.swift
@@ -8,16 +8,48 @@
 import UIKit
 
 class TravelTableViewCell: UITableViewCell {
-
+    
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var travelLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
+        travelLabel.backgroundColor = .systemYellow
+        travelLabel.text = "텍스트"
+        travelLabel.font = .boldSystemFont(ofSize: 17)
+        travelLabel.numberOfLines = 0
+        travelLabel.backgroundColor = .clear
+        
+        dateLabel.backgroundColor = .clear
     }
     
+    override func prepareForReuse() {
+        dateLabel.text = nil
+        travelLabel.text = nil
+        backgroundColor = .clear
+    }
+    
+    func configure(with travel: Travel) {
+        configureDateLabel(name: travel.name)
+        configureTravelLabel(overview: travel.overview)
+        configureBackground(isLiked: travel.like)
+    }
+    
+    private func configureDateLabel(name: String) {
+        dateLabel.text = name
+
+    }
+    
+    private func configureTravelLabel(overview: String) {
+        travelLabel.text = overview
+    }
+    
+    private func configureBackground(isLiked: Bool) {
+        if isLiked {
+            backgroundColor = .yellow
+        } else {
+            backgroundColor = .clear
+        }
+    }
 }
