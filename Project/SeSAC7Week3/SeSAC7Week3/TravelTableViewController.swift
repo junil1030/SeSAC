c6218d66ccdeef979c79b8dd0fb8ff3a8d964945
junil1030 <dccrdseo@naver.com>
2025-07-14 15:32:56 +0900

    feat: TableView 구성

--- Project/SeSAC7Week3/SeSAC7Week3/TravelTableViewController.swift
+++ Project/SeSAC7Week3/SeSAC7Week3/TravelTableViewController.swift
@@ -7,83 +7,60 @@
 
 import UIKit
 
+struct Travel {
+    let name: String
+    let overview: String
+    let date: String
+    let like: Bool
+}
+
 class TravelTableViewController: UITableViewController {
 
+    let nickname = "고래밥"
+    let formatter = DateFormatter()
+    
+    let list = [
+        Travel(name: "서울", overview: "더현대서울 좋아요", date: "250323", like: false),
+        Travel(name: "대전", overview: "성심당 좋아요", date: "23018", like: false),
+        Travel(name: "평택", overview: "뭐가 좋지", date: "250714", like: true),
+        Travel(name: "부산", overview: "톤쇼우 좋아요", date: "241030", like: false),
+        Travel(name: "강릉", overview: "쏠비치 좋아요", date: "250806", like: false),
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        setBackground()
+        
+        // XIB Cell로 구성하는 순간, 필요한 코드
+        let xib = UINib(nibName: "TravelTableViewCell", bundle: nil)
+        tableView.register(xib, forCellReuseIdentifier: "TravelTableViewCell")
+        
+        tableView.rowHeight = UITableView.automaticDimension
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+
+        return list.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TravelTableViewCell", for: indexPath) as! TravelTableViewCell
+    
+        cell.configure(with: list[indexPath.row])
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
+//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return UITableView.automaticDimension
+//    }
 
 }
