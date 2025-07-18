ab97a462202a498144b4b0336bd5226aa186e25e
junil1030 <dccrdseo@naver.com>
2025-07-18 22:48:17 +0900

    feat: Segment Index에 따라 필터링된 도시 정보들 reload하도록 작성, 검색 기능은 추가하지 않은 상태

--- Assignment/250715/task01/task01/Views/Cells/CityInfoTableViewCell.swift
+++ Assignment/250715/task01/task01/Views/Cells/CityInfoTableViewCell.swift
@@ -8,16 +8,43 @@
 import UIKit
 
 class CityInfoTableViewCell: UITableViewCell {
+    
+    static let identifier = "CityInfoTableViewCell"
 
+    @IBOutlet var backgroundImageView: UIImageView!
+    @IBOutlet var cityNameLabel: UILabel!
+    @IBOutlet var explainBackgroundView: UIView!
+    @IBOutlet var explainLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
+        configure()
     }
     
+    private func configure() {
+        contentView.layer.cornerRadius = 20
+        contentView.layer.maskedCorners = [
+            .layerMinXMinYCorner,
+            .layerMaxXMaxYCorner
+        ]
+        contentView.clipsToBounds = true
+        
+        explainBackgroundView.backgroundColor = .black.withAlphaComponent(0.5)
+        
+        cityNameLabel.textColor = .white
+        cityNameLabel.font = .boldSystemFont(ofSize: 28)
+        
+        explainLabel.textColor = .white
+        explainLabel.font = .systemFont(ofSize: 20)
+        
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.clipsToBounds = true
+    }
+    
+    func configureData(with row: City) {
+        backgroundImageView.setImage(with: row.city_image)
+        cityNameLabel.text = "\(row.city_name) | \(row.city_english_name)"
+        explainLabel.text = row.city_explain
+    }
 }
--- Assignment/250715/task01/task01/Views/CityInfoTableViewController.swift
+++ Assignment/250715/task01/task01/Views/CityInfoTableViewController.swift
@@ -9,22 +9,67 @@ import UIKit
 
 class CityInfoTableViewController: UITableViewController {
 
+    private let cityInfo = CityInfo().city
+    private var filteredCityInfo: [City] = []
+    
+    @IBOutlet var searchTextField: UITextField!
+    @IBOutlet var segment: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         navigationItem.title = "인기 도시"
+        tableView.rowHeight = 150
+        filteredCityInfo = cityInfo
+
+    }
+    
+    private func registerXib() {
+        let xib = UINib(nibName: CityInfoTableViewCell.identifier, bundle: nil)
+        tableView.register(xib, forCellReuseIdentifier: CityInfoTableViewCell.identifier)
+    }
+    
+    private func setupSegment() {
+        segment.removeAllSegments()
+        let segmentItems = ["모두", "국내", "해외"]
+        for (index, title) in segmentItems.enumerated() {
+            segment.insertSegment(withTitle: title, at: index, animated: false)
+        }
+        segment.selectedSegmentIndex = 0
+        segment.addTarget(self, action: #selector(segmentChanged(_:)), for: .valueChanged)
+    }
+
+    private func filterCity(for index: Int) {
+        switch index {
+        case 1:
+            filteredCityInfo = cityInfo.filter { $0.domestic_travel }
+        case 2:
+            filteredCityInfo = cityInfo.filter { !$0.domestic_travel }
+        default:
+            filteredCityInfo = cityInfo
+        }
+        tableView.reloadData()
+    }
+    
+    @objc func segmentChanged(_ sender: UISegmentedControl) {
+        filterCity(for: sender.selectedSegmentIndex)
     }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 0
+        return filteredCityInfo.count
     }
-
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: CityInfoTableViewCell.identifier, for: indexPath) as! CityInfoTableViewCell
+        
+        cell.configureData(with: filteredCityInfo[indexPath.row])
+        
+        return cell
+    }
 }
