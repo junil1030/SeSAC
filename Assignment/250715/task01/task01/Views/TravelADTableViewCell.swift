2511eaad8a4b3fe74837e37bffec9c393c41b31e
junil1030 <dccrdseo@naver.com>
2025-07-18 16:21:30 +0900

    feat: City Cell과 AD Cell에 대한 xib, swift 파일 생성

---
+++ Assignment/250715/task01/task01/Views/TravelADTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelADTableViewCell.swift
+//  task01
+//
+//  Created by 서준일 on 7/18/25.
+//
+
+import UIKit
+
+class TravelADTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Assignment/250715/task01/task01/Views/TravelCityTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TravelCityTableViewCell.swift
+//  task01
+//
+//  Created by 서준일 on 7/18/25.
+//
+
+import UIKit
+
+class TravelCityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
