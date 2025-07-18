359bc8b899843b86be4ff15d2bf60df3c20f24c1
junil1030 <dccrdseo@naver.com>
2025-07-18 17:03:58 +0900

    feat: Cell 작성 및 로직 추가

--- Assignment/250715/task01/task01/Views/Cells/TravelADTableViewCell.swift
+++ Assignment/250715/task01/task01/Views/Cells/TravelADTableViewCell.swift
@@ -9,15 +9,39 @@ import UIKit
 
 class TravelADTableViewCell: UITableViewCell {
 
+    @IBOutlet var backgroundColorView: UIView!
+    @IBOutlet var adContentLabel: UILabel!
+    @IBOutlet var adLabel: UILabel!
+    
+    private let pastelColors: [UIColor] = [
+        UIColor(red: 255/255, green: 182/255, blue: 193/255, alpha: 1.0),
+        UIColor(red: 173/255, green: 216/255, blue: 230/255, alpha: 1.0),
+        UIColor(red: 221/255, green: 160/255, blue: 221/255, alpha: 1.0),
+        UIColor(red: 144/255, green: 238/255, blue: 144/255, alpha: 1.0),
+        UIColor(red: 255/255, green: 218/255, blue: 185/255, alpha: 1.0),
+        UIColor(red: 255/255, green: 255/255, blue: 224/255, alpha: 1.0),
+        UIColor(red: 230/255, green: 230/255, blue: 250/255, alpha: 1.0),
+        UIColor(red: 240/255, green: 248/255, blue: 255/255, alpha: 1.0),
+        UIColor(red: 250/255, green: 240/255, blue: 230/255, alpha: 1.0),
+        UIColor(red: 255/255, green: 240/255, blue: 245/255, alpha: 1.0)
+    ]
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        backgroundColorView.backgroundColor = pastelColors.randomElement() ?? UIColor.lightGray
+        backgroundColorView.layer.cornerRadius =  15
+        adLabel.layer.cornerRadius = 5
+        adLabel.clipsToBounds = true
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        adContentLabel.text = nil
     }
     
+    func configure(with travel: Travel) {
+        adContentLabel.text = travel.title
+    }
 }
--- Assignment/250715/task01/task01/Views/Cells/TravelCityTableViewCell.swift
+++ Assignment/250715/task01/task01/Views/Cells/TravelCityTableViewCell.swift
@@ -6,18 +6,90 @@
 //
 
 import UIKit
+import Kingfisher
 
 class TravelCityTableViewCell: UITableViewCell {
 
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subTitleLabel: UILabel!
+    @IBOutlet var reviewStarImageVIew: [UIImageView]!
+    @IBOutlet var reviewLabel: UILabel!
+    @IBOutlet var saveLabel: UILabel!
+    @IBOutlet var travelImage: UIImageView!
+    @IBOutlet var likeButton: UIButton!
+    
+    private var isLiked: Bool = false
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        updateLikeButtonAppearance()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        titleLabel.text = nil
+        subTitleLabel.text = nil
+        reviewLabel.text = nil
+        saveLabel.text = nil
+        travelImage.image = nil
     }
     
+    func configure(with travel: Travel) {
+        titleLabel.text = travel.title
+        subTitleLabel.text = travel.description
+        setRatingImage(travel.grade)
+        
+        if let imageUrlString = travel.travel_image {
+            if let imageURL = URL(string: imageUrlString) {
+                travelImage.kf.setImage(with: imageURL)
+            }
+        }
+        
+        if let isLiked = travel.like {
+            self.isLiked = isLiked
+        }
+        updateLikeButtonAppearance()
+        
+        if let review = travel.review {
+            reviewLabel.text = "(\(review.formattedWithComma))"
+        }
+        
+        if let save = travel.save {
+            saveLabel.text = "저장 \(save.formattedWithComma)"
+        }
+        
+    }
+    
+    private func setRatingImage(_ rating: Double?) {
+        guard let reviewRate = rating else {
+            for imageView in reviewStarImageVIew {
+                imageView.tintColor = .systemGray
+            }
+            return
+        }
+        
+        let yellowStar = Int(reviewRate)
+        
+        for (index, imageView) in reviewStarImageVIew.enumerated() {
+            if index < yellowStar {
+                imageView.tintColor = .systemYellow
+            } else {
+                imageView.tintColor = .systemGray
+            }
+        }
+    }
+    
+    private func updateLikeButtonAppearance() {
+        if isLiked {
+            likeButton.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+        } else {
+            likeButton.setImage(UIImage(systemName: "heart"), for: .normal)
+        }
+    }
+    
+    @IBAction func likeButtonTapped(_ sender: UIButton) {
+        isLiked.toggle()
+    }
 }
