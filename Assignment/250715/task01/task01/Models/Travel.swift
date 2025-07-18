1d6699bc623de4eded1b4dc802714435c0687d87
junil1030 <dccrdseo@naver.com>
2025-07-18 16:09:33 +0900

    feat: Data Model및 Info 추가

---
+++ Assignment/250715/task01/task01/Models/Travel.swift
@@ -0,0 +1,19 @@
+//
+//  Travel.swift
+//  task01
+//
+//  Created by 서준일 on 7/11/25.
+//
+
+import Foundation
+
+struct Travel {
+    let title: String
+    let description: String?
+    let travel_image: String?
+    let grade: Double?
+    let review: Int?
+    let save: Int?
+    let like: Bool?
+    let ad: Bool
+}
