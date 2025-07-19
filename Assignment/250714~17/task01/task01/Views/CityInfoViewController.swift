06b29b57340203113877309ce2085de54d3e62f5
junil1030 <dccrdseo@naver.com>
2025-07-19 22:47:10 +0900

    refactor: CollectionView의 FlowLayout 설정을 Delegate로 사용하지 않고 ViewDidLoad 시점에 적용하도록 변경

--- Assignment/250714~17/task01/task01/Views/CityInfoViewController.swift
+++ Assignment/250714~17/task01/task01/Views/CityInfoViewController.swift
@@ -14,6 +14,11 @@ class CityInfoViewController: UIViewController {
 
     @IBOutlet var segment: UISegmentedControl!
     @IBOutlet var collectionView: UICollectionView!
+
+    let sectionInset: CGFloat = 8
+    let minimumInteritemSpacing: CGFloat = 8
+    let minimumLineSpacing: CGFloat = 8
+    let column: CGFloat = 2
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +29,7 @@ class CityInfoViewController: UIViewController {
         registerXib()
         setupDelegate()
         setupSegment()
+        setupCollectionView()
     }
     
     private func registerXib() {
@@ -44,6 +50,21 @@ class CityInfoViewController: UIViewController {
         }
         segment.selectedSegmentIndex = 0
         segment.addTarget(self, action: #selector(segmentChanged(_:)), for: .valueChanged)
+    }
+    
+    private func setupCollectionView() {
+        let layout = UICollectionViewFlowLayout()
+        
+        let collectionViewWidth = collectionView.bounds.width
+        let cellWidth = (collectionViewWidth - (sectionInset * 2) - minimumLineSpacing - minimumInteritemSpacing) / column
+        let cellHeight = cellWidth * 1.4
+        
+        layout.itemSize = CGSize(width: cellWidth, height: cellHeight)
+        layout.sectionInset = UIEdgeInsets(top: 0, left: sectionInset, bottom: 0, right: sectionInset)
+        layout.minimumInteritemSpacing = minimumInteritemSpacing
+        layout.minimumLineSpacing = minimumLineSpacing
+        
+        collectionView.collectionViewLayout = layout
     }
     
     private func filterCity(for index: Int) {
@@ -78,14 +99,18 @@ extension CityInfoViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CityInfoCollectionViewCell.identifier, for: indexPath) as! CityInfoCollectionViewCell
         
-        cell.configureData(with: filteredCityInfo[indexPath.row])
+        cell.configureData(with: filteredCityInfo[indexPath.item])
         return cell
     }
 }
 
-extension CityInfoViewController: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = (collectionView.bounds.width - 30) / 2
-        return CGSize(width: width, height: width * 1.4)
-    }
-}
+//extension CityInfoViewController: UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//        let width = (collectionView.bounds.width - 30) / 2
+//        return CGSize(width: width, height: width * 1.4)
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+//        UIEdgeInsets(top: 5, left: 10, bottom: 0, right: 10)
+//    }
+//}
