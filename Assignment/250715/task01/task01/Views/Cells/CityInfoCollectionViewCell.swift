74eb210c45ac2b889b7f10be988c87cb5093306b
junil1030 <dccrdseo@naver.com>
2025-07-19 15:11:45 +0900

    feat: 15회차 과제 추가 (CollectionViewCell 추가 및 Main Storyboard에서 ViewController에 segment control, collection view 추가

---
+++ Assignment/250715/task01/task01/Views/Cells/CityInfoCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CityInfoCollectionViewCell.swift
+//  task01
+//
+//  Created by 서준일 on 7/19/25.
+//
+
+import UIKit
+
+class CityInfoCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
