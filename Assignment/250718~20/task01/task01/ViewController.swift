54b63d4345accf87e5497ce35d5ae8b88c9ab237
junil1030 <dccrdseo@naver.com>
2025-07-20 14:50:08 +0900

    Init Assignment: 16회차 주말과제 프로젝트 생성

---
+++ Assignment/250718~20/task01/task01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task01
+//
+//  Created by 서준일 on 7/20/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
