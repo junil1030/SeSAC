a6f44217c3fe68c4784998f3bef3b612cd492fe1
junil1030 <dccrdseo@naver.com>
2025-07-20 22:05:13 +0900

    feat: 회색 컬러 커스텀으로 추가

---
+++ Assignment/250718~20/task01/task01/Utills/CustomColor.swift
@@ -0,0 +1,12 @@
+//
+//  CustomColor.swift
+//  task01
+//
+//  Created by 서준일 on 7/20/25.
+//
+
+import UIKit
+
+struct CustomColor {
+    static let grayColor = UIColor(red: 231/255, green: 231/255, blue: 231/255, alpha: 1)
+}
