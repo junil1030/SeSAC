b0899f10092d00d088f0a416eed623823d3bb990
junil1030 <dccrdseo@naver.com>
2025-07-20 17:53:35 +0900

    Chat 화면에 대한 뷰컨트롤러 소스파일 추가

---
+++ Assignment/250718~20/task01/task01/Views/ChatViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ChatViewController.swift
+//  task01
+//
+//  Created by 서준일 on 7/20/25.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
