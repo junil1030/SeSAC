c1a28e6a39594a0259e3c07c2029967bb348421d
junil1030 <dccrdseo@naver.com>
2025-07-20 17:53:00 +0900

    feat: 시간을 format해주는 extension 추가

---
+++ Assignment/250718~20/task01/task01/Utills/String+Extension.swift
@@ -0,0 +1,40 @@
+//
+//  String+Extension.swift
+//  task01
+//
+//  Created by 서준일 on 7/20/25.
+//
+
+import Foundation
+
+enum FormatCase: String {
+    case yyMMdd = "yy.MM.dd"
+    case HHmma = "HH:mm a"
+}
+
+extension String {
+    func dateFormatted(to format: FormatCase) -> String {
+        let inputFormatter = DateFormatter()
+        inputFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+        inputFormatter.locale = Locale(identifier: "ko_KR")
+
+        let outputFormatter = DateFormatter()
+        outputFormatter.dateFormat = format.rawValue
+        outputFormatter.locale = Locale(identifier: "ko_KR")
+
+        guard let date = inputFormatter.date(from: self) else {
+            return self
+        }
+
+        let result = outputFormatter.string(from: date)
+
+        if format == .HHmma {
+            let components = result.components(separatedBy: " ")
+            if components.count == 2 {
+                return "\(components[1]) \(components[0])"
+            }
+        }
+
+        return result
+    }
+}
