4bd31851f848690c3131f108c71c38bea3eb20b6
junil1030 <dccrdseo@naver.com>
2025-07-21 09:49:10 +0900

    feat: Init Project Week4

---
+++ Project/SeSAC7Week4Codebase/SeSAC7Week4Codebase/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC7Week4Codebase
+//
+//  Created by 서준일 on 7/21/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
