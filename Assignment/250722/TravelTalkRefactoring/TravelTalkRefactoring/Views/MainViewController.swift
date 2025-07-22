f88812d07721c90183f94eadd042f6c0724d3ed8
junil1030 <dccrdseo@naver.com>
2025-07-22 16:22:41 +0900

    feat: UISetupProtocol 채택, SearchBar와 CollectionView 생성하여 Constraints 설정

--- Assignment/250722/TravelTalkRefactoring/TravelTalkRefactoring/Views/MainViewController.swift
+++ Assignment/250722/TravelTalkRefactoring/TravelTalkRefactoring/Views/MainViewController.swift
@@ -6,14 +6,84 @@
 //
 
 import UIKit
+import SnapKit
 
-class MainViewController: UIViewController {
+class MainViewController: UIViewController, UISetupProtocol {
+    
+    let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.backgroundImage = UIImage()
+        searchBar.placeholder = "친구 이름을 검색해보세요"
+        searchBar.showsBookmarkButton = false
+        return searchBar
+    }()
+    
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        return collectionView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
+        setupConstraints()
+        setupActions()
+        setupDelegate()
     }
 
+    func setupUI() {
+        view.backgroundColor = .white
+        
+        navigationItem.title = "TRAVEL TALK"
+        navigationItem.backButtonTitle = ""
+        
+        view.addSubview(searchBar)
+        view.addSubview(collectionView)
+    }
+    
+    func setupConstraints() {
+        searchBar.snp.makeConstraints { make in
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide.snp.horizontalEdges)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.height.equalTo(44)
+        }
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(searchBar.snp.bottom).offset(3)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide.snp.horizontalEdges)
+            make.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+        }
+    }
+    
+    func setupActions() {
+        
+    }
+    
+    func setupDelegate() {
+        searchBar.delegate = self
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+}
 
+extension MainViewController: UISearchBarDelegate {
+    
+}
+
+extension MainViewController: UICollectionViewDelegate {
+    
+}
+
+extension MainViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
 }
 
