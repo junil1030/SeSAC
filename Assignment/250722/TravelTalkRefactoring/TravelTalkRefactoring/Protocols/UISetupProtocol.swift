8948eab29555ea994ba372dbb645f8a01b028ca8
junil1030 <dccrdseo@naver.com>
2025-07-22 16:21:25 +0900

    feat: UISetup을 위한 Protocol 추가

---
+++ Assignment/250722/TravelTalkRefactoring/TravelTalkRefactoring/Protocols/UISetupProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  UISetupProtocol.swift
+//  TravelTalkRefactoring
+//
+//  Created by 서준일 on 7/22/25.
+//
+
+import Foundation
+
+protocol UISetupProtocol {
+    func setupUI()
+    func setupConstraints()
+    func setupActions()
+}
