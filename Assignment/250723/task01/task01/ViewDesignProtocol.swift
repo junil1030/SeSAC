99ab6d5acfb4955d886e18e5bd264737e7938f5a
junil1030 <dccrdseo@naver.com>
2025-07-23 17:58:24 +0900

    feat: ViewDesignProtocol 생성

---
+++ Assignment/250723/task01/task01/ViewDesignProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  ViewDesignProtocol.swift
+//  task01
+//
+//  Created by 서준일 on 7/23/25.
+//
+
+import Foundation
+
+protocol ViewDesignProtocol: AnyObject {
+    
+    func configureHierarchy()
+    func configureLayout()
+    func configureView()
+    
+}
