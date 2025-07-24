da24bd23d87b9783aaeabe7ca4bd4a6f0e6e5592
junil1030 <dccrdseo@naver.com>
2025-07-24 18:13:07 +0900

    feat: Lotto 데이터를 받아올 구조체 생성

---
+++ Assignment/250723~24/task01/task01/Models/Lotto.swift
@@ -0,0 +1,17 @@
+//
+//  Lotto.swift
+//  task01
+//
+//  Created by 서준일 on 7/24/25.
+//
+
+struct Lotto: Decodable {
+    let drwNoDate: String
+    let drwtNo1: Int
+    let drwtNo2: Int
+    let drwtNo3: Int
+    let drwtNo4: Int
+    let drwtNo5: Int
+    let drwtNo6: Int
+    let bnusNo: Int
+}
