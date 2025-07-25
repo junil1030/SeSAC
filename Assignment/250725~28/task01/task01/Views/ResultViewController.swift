a108f49a0aac1798cfaed2cdec4ed984b0824b03
junil1030 <dccrdseo@naver.com>
2025-07-25 16:10:24 +0900

    feat: 기본적으로 필요한 결과 레이블, 필터 레이블, 콜렉션 뷰 생성후 Layout 설정

--- Assignment/250725~28/task01/task01/Views/ResultViewController.swift
+++ Assignment/250725~28/task01/task01/Views/ResultViewController.swift
@@ -6,10 +6,36 @@
 //
 
 import UIKit
+import SnapKit
 
 class ResultViewController: BaseViewController {
     
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.keyboardDismissMode = .onDrag
+        return collectionView
+    }()
+    
+    let searchResultCountLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .green
+        label.textAlignment = .left
+        label.font = .boldSystemFont(ofSize: 15)
+        return label
+    }()
+    
+    let filterStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.alignment = .fill
+        return stackView
+    }()
+    
     let searchKeyword: String
+    let searchResultCount = 0
     
     init(searchKeyword: String) {
         self.searchKeyword = searchKeyword
@@ -25,9 +51,30 @@ class ResultViewController: BaseViewController {
         
     }
     
+    override func configureHierarchy() {
+        super.configureHierarchy()
+        view.addSubview(searchResultCountLabel)
+        view.addSubview(collectionView)
+    }
+    
+    override func configureLayout() {
+        super.configureLayout()
+        searchResultCountLabel.snp.makeConstraints { make in
+            make.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+            make.height.equalTo(32)
+        }
+        
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(searchResultCountLabel.snp.bottom)
+            make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+    
     override func configureView() {
         super.configureView()
         
         navigationItem.title = searchKeyword
+        
+        searchResultCountLabel.text = "Test"
     }
 }
