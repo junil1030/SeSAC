cb5a1eb55c2b73b16a9acde206918e92333544a8
junil1030 <dccrdseo@naver.com>
2025-07-26 15:16:04 +0900

    feat: 기본 APIService 클래스 생성

---
+++ Assignment/250725~28/task01/task01/Network/APIService.swift
@@ -0,0 +1,40 @@
+//
+//  APIService.swift
+//  task01
+//
+//  Created by 서준일 on 7/26/25.
+//
+
+import Foundation
+import Alamofire
+
+class APIService {
+    static let shared = APIService()
+    
+    private init() {}
+    
+    func searchProduct(keyword: String, completion: @escaping (ShoppingResponse?) -> Void) {
+        let url = "https://openapi.naver.com/v1/search/shop"
+        
+        let headers: HTTPHeaders = [
+            "X-Naver-Client-Id": Bundle.main.object(forInfoDictionaryKey: "X-Naver-Client-Id") as! String,
+            "X-Naver-Client-Secret": Bundle.main.object(forInfoDictionaryKey: "X-Naver-Client-Secret") as! String
+        ]
+        
+        let parameters: [String: Any] = [
+            "query": keyword,
+            "display": 100,
+            "start": 1
+        ]
+        
+        AF.request(url, parameters: parameters, headers: headers)
+            .responseDecodable(of: ShoppingResponse.self) { response in
+                switch response.result {
+                case .success(let data):
+                    completion(data)
+                case .failure:
+                    completion(nil)
+                }
+            }
+    }
+}
