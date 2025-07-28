f880e4de515b0fb59dca8bd2d166a5b32728902e
junil1030 <dccrdseo@naver.com>
2025-07-28 12:05:08 +0900

    feat: 0728 BaseViewController 생성

---
+++ Project/SeSAC7Week4Codebase/SeSAC7Week4Codebase/Base/BaseViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BaseViewController.swift
+//  SeSAC7Week4Codebase
+//
+//  Created by 서준일 on 7/28/25.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    
+}
