49dc9dc779730175daac33f6f94f0509c67b3426
junil1030 <dccrdseo@naver.com>
2025-07-28 21:04:08 +0900

    feat: Enum으로 네트워크 상태를 관리하고 연결 여부와 상태를 나타냄

---
+++ Assignment/250725~28/task01/task01/Network/NetworkManager.swift
@@ -0,0 +1,75 @@
+//
+//  NetworkManager.swift
+//  task01
+//
+//  Created by 서준일 on 7/28/25.
+//
+
+import Foundation
+import Alamofire
+
+enum NetworkStatus {
+    case wifi
+    case cellular
+    case disconnected
+    case unknown
+    
+    var isConnected: Bool {
+        switch self {
+        case .wifi, .cellular:
+            return true
+        case .disconnected, .unknown:
+            return false
+        }
+    }
+    
+    var description: String {
+        switch self {
+        case .wifi:
+            return "WiFi 연결됨"
+        case .cellular:
+            return "셀룰러 연결됨"
+        case .disconnected:
+            return "인터넷 연결 안됨"
+        case .unknown:
+            return "연결 상태 알 수 없음"
+        }
+    }
+}
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private let reachabilityManager = NetworkReachabilityManager()
+    
+    private init() {}
+    
+    var currentStatus: NetworkStatus {
+        guard let reachability = reachabilityManager else { return .unknown }
+        
+        switch reachability.status {
+        case .notReachable:
+            return .disconnected
+        case .reachable(.ethernetOrWiFi):
+            return .wifi
+        case .reachable(.cellular):
+            return .cellular
+        case .unknown:
+            return .unknown
+        }
+    }
+    
+    var isConnected: Bool {
+        return currentStatus.isConnected
+    }
+    
+    func startNetworkMonitoring() {
+        reachabilityManager?.startListening { [weak self] status in
+            let _ = self?.currentStatus
+        }
+    }
+    
+    func stopNetworkMonitoring() {
+        reachabilityManager?.stopListening()
+    }
+}
