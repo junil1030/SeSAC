2de0821c936d690f547d36b8b5669398d26f2e70
junil1030 <dccrdseo@naver.com>
2025-07-28 16:37:32 +0900

    refactor: 기존 뷰컨트롤러에서 관리하던 뷰를 따로 AuthView로 이전

--- Project/SeSAC7Week4Codebase/SeSAC7Week4Codebase/Fourth/AuthViewController.swift
+++ Project/SeSAC7Week4Codebase/SeSAC7Week4Codebase/Fourth/AuthViewController.swift
@@ -13,47 +13,28 @@ class AuthViewController: UIViewController {
     // 인스턴스 프로퍼티와 인스턴스 메서드는 인스턴스가 만들어진 시점 이후에만 사용이 가능함
     // 근데 지금 아래와 같은 코드는 인스턴스가 만들어지지도 않았는데 접근하려고 하니까 말이 안된다고 하는 거임
     // self가 아직 다 만들어지지 않았는데
-    let emailTextField: UITextField = {
-        print("emailTextFiled 생성")
-        let textField = PurpleTextField(placeholder: "이메일을 작성해주세요", keyboard: .emailAddress)
-        return textField
-    }()
     
-    let passwordTextField = {
-        print(#function)
-        let textField = PurpleTextField(placeholder: "비밀번호를 작성해주세요", keyboard: .default)
-        textField.isSecureTextEntry = true
-        return textField
-    }()
+    let mainView = AuthView()
     
-    lazy var ageTextField = {
-        let textField = PurpleTextField(placeholder: "나이를 선택해주세요", keyboard: .numberPad)
-        textField.inputView = picker
-        return textField
-    }()
-    
-    let picker = UIDatePicker()
+    override func loadView() {
+        self.view = mainView
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         print("viewdidload 시작")
-        configureHierarchy()
-        configureLayout()
-        configureView()
         
-        passwordTextField.delegate = self
-        
-        picker.preferredDatePickerStyle = .wheels
-        picker.addTarget(self, action: #selector(datePickerValueChanged), for: .valueChanged)
-        ageTextField.inputView = picker
+        mainView.passwordTextField.delegate = self
+
+        mainView.picker.addTarget(self, action: #selector(datePickerValueChanged), for: .valueChanged)
         
         print("viewdidload 끝")
     }
     
     @objc func datePickerValueChanged() {
         print(#function)
-        ageTextField.text = "\(picker.date)"
+        mainView.ageTextField.text = "\(mainView.picker.date)"
         
     }
 }
@@ -68,40 +49,4 @@ extension AuthViewController: UITextFieldDelegate {
     }
     
     
-}
-
-//MARK: - UI 속성 정의
-extension AuthViewController: ViewDesignProtocol {
-    
-    func configureHierarchy() {
-        view.addSubview(emailTextField)
-        view.addSubview(passwordTextField)
-        view.addSubview(ageTextField)
-    }
-    
-    func configureLayout() {
-        emailTextField.snp.makeConstraints { make in
-            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
-            make.top.equalTo(view.safeAreaLayoutGuide).offset(20)
-            make.height.equalTo(50)
-        }
-        
-        passwordTextField.snp.makeConstraints { make in
-            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
-            make.top.equalTo(emailTextField.snp.bottom).offset(20)
-            make.height.equalTo(50)
-        }
-        
-        ageTextField.snp.makeConstraints { make in
-            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
-            make.top.equalTo(passwordTextField.snp.bottom).offset(20)
-            make.height.equalTo(50)
-        }
-    }
-    
-    func configureView() {
-        view.backgroundColor = .white
-
-        
-    }
 }
