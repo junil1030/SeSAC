122c29b2930437224f9f2a5ef49c8a164a763157
junil1030 <dccrdseo@naver.com>
2025-07-28 16:35:13 +0900

    feat: BaseView, BaseCell 생성, 뷰 컨트롤러와 뷰의 기능을 나누기 위해 AuthView 생성

---
+++ Project/SeSAC7Week4Codebase/SeSAC7Week4Codebase/Fourth/AuthView.swift
@@ -0,0 +1,8 @@
+//
+//  AuthView.swift
+//  SeSAC7Week4Codebase
+//
+//  Created by 서준일 on 7/28/25.
+//
+
+import Foundation
---
+++ Project/SeSAC7Week4Codebase/SeSAC7Week4Codebase/Fourth/Base/BaseView.swift
@@ -0,0 +1,20 @@
+//
+//  BaseView.swift
+//  SeSAC7Week4Codebase
+//
+//  Created by 서준일 on 7/28/25.
+//
+
+import UIKit
+
+class BaseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
