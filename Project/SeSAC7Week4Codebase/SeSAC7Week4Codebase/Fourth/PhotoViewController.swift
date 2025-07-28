67efc2e36d22264c2e3be0612bf70b558793da13
junil1030 <dccrdseo@naver.com>
2025-07-28 16:39:46 +0900

    test: PhtoViewController에서 상속 관계에서의 호출 순서 테스팅

--- Project/SeSAC7Week4Codebase/SeSAC7Week4Codebase/Fourth/PhotoViewController.swift
+++ Project/SeSAC7Week4Codebase/SeSAC7Week4Codebase/Fourth/PhotoViewController.swift
@@ -7,23 +7,29 @@
 
 import UIKit
 
-class PhotoViewController: UIViewController {
+class PhotoViewController: BaseViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        print("Photo", #function)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func setupHierachy() {
+        super.setupHierachy()
+        
+        print("Photo", #function)
     }
-    */
 
+    override func setupLayout() {
+        super.setupLayout()
+        
+        print("Photo", #function)
+    }
+
+    override func setupView() {
+        super.setupView()
+        
+        print("Photo", #function)
+    }
 }
