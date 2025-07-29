f704cfb5d7102c9e67d7fc2b69ee0b63431a5e97
junil1030 <dccrdseo@naver.com>
2025-07-29 09:38:19 +0900

    feat: 5주차 프로젝트 생성

---
+++ Project/SeSAC7Week5/SeSAC7Week5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC7Week5
+//
+//  Created by 서준일 on 7/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
