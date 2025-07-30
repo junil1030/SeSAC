a050530e55a181e27b6b05f103defc2dbe04c9ae
junil1030 <dccrdseo@naver.com>
2025-07-30 18:57:20 +0900

    refactor: networkManger를 생성자에서 받아오도록 Base 구조 수정

--- Assignment/250725~28/task01/task01/Base/BaseViewController.swift
+++ Assignment/250725~28/task01/task01/Base/BaseViewController.swift
@@ -31,6 +31,16 @@ protocol ViewDesignProtocol: AnyObject {
 }
 
 class BaseViewController: UIViewController, ViewDesignProtocol {
+    private let networkManager: NetworkManagerProtocol
+    
+    init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?, networkManager: NetworkManagerProtocol) {
+        self.networkManager = networkManager
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -66,7 +76,7 @@ class BaseViewController: UIViewController, ViewDesignProtocol {
     }
     
     func checkNetworkConnection() -> Bool {
-        let status = NetworkManager.shared.currentStatus
+        let status = networkManager.currentStatus
         
         if !status.isConnected {
             showNetworkAlert(status: status)
