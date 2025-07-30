21dd87e7077fbc5ed2ba75161cc6034303f4026c
junil1030 <dccrdseo@naver.com>
2025-07-30 18:42:37 +0900

    feat: DIContainer클래스를 생성 후 생성 중에 NetworkManager의 인스턴스를 생성하여 보관 / 이 클래스를 관리하는 DIContaninerManager를 통해 reslove함수로 인스턴스 받아오도록 함

--- Assignment/250725~28/task01/task01/DI/DIContainer.swift
+++ Assignment/250725~28/task01/task01/DI/DIContainer.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+protocol DIprotocol {
+    func resolve<T>(_ type: T.Type) -> T
+}
+
+class DIContainer: DIprotocol {
+    private let networkManager: NetworkManagerProtocol
+    
+    init() {
+        self.networkManager = NetworkManager()
+    }
+    
+    func resolve<T>(_ type: T.Type) -> T {
+        switch type {
+        case is NetworkManagerProtocol.Type:
+            return networkManager as! T
+        default:
+            fatalError("Cannot Resolve \(type)")
+        }
+    }
+}
+
+class DIContainerManager {
+    static let shared = DIContainer()
+    
+    private init() {}
+}
