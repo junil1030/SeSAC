efc338ba803264623cbd75eabf9fe8092e0336e8
junil1030 <dccrdseo@naver.com>
2025-08-04 09:45:15 +0900

    chore: Init Projcet

---
+++ Project/SeSAC7Week6/SeSAC7Week6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SeSAC7Week6
+//
+//  Created by 서준일 on 8/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
