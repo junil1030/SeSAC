1703ab4b96bf7d99b0f2d8d31a76729c39b089b2
junil1030 <dccrdseo@naver.com>
2025-08-07 22:41:25 +0900

    feat: textField에 들어오는 값의 유효성을 검사하는 함수 추가

--- Assignment/250807/MVVMBasic/MVVMBasic/UIViewController+Extension.swift
+++ Assignment/250807/MVVMBasic/MVVMBasic/UIViewController+Extension.swift
@@ -7,6 +7,29 @@
 
 import UIKit
 
+enum InputError: Error {
+    case empty
+    case containsWhitespace
+    case notNumeric
+    case outOfRange(min: Any, max: Any)
+    case unknown
+    
+    var message: String {
+        switch self {
+        case .empty:
+            return "입력된 숫자가 없어요."
+        case .containsWhitespace:
+            return "공백없이 입력해주세요."
+        case .notNumeric:
+            return "숫자만 입력해주세요."
+        case .outOfRange(let min, let max):
+            return "\(min) 부터 \(max) 사이의 숫자만 입력해주세요."
+        case .unknown:
+            return "알 수 없는 오류가 발생했어요."
+        }
+    }
+}
+
 extension UIViewController {
     
     func showToast(message: String) {
@@ -18,5 +41,14 @@ extension UIViewController {
         }
     }
     
-    
+    // func validateUserData<T>(_ text: String?, min: T, max: T) throws -> T where T: LosslessStringConvertible & Comparable {
+    // 이렇게 뒤에 where를 붙여서 사용할 수도 있음
+    func validateUserData<T: LosslessStringConvertible & Comparable>(_ text: String?, min: T, max: T) throws -> T {
+        guard let text = text, !text.isEmpty else { throw InputError.empty }
+        guard !text.contains(" ") else { throw InputError.containsWhitespace }
+        guard let number = T(text) else { throw InputError.notNumeric }
+        guard number >= min && number <= max else { throw InputError.outOfRange(min: min, max: max) }
+        
+        return number
+    }
 }
