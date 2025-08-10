803370817506763d4d4a6651813d2f82e2aa5ee7
junil1030 <dccrdseo@naver.com>
2025-08-10 23:35:56 +0900

    feat: 유효성 검증을 위한 프로토콜 추가

--- Assignment/250807~08/MVVMBasic/MVVMBasic/ValidateProtocol.swift
+++ Assignment/250807~08/MVVMBasic/MVVMBasic/ValidateProtocol.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+protocol ValidateProtocol {
+    
+    func validateUserData<T: LosslessStringConvertible & Comparable>(_ text: String?, min: T, max: T) -> Result<T, InputError>
+}
+
+extension ValidateProtocol {
+    func validateUserData<T: LosslessStringConvertible & Comparable>(_ text: String?, min: T, max: T) -> Result<T, InputError> {
+        guard let text = text, !text.isEmpty else { return .failure(.empty) }
+        guard !text.contains(" ") else { return .failure(.containsWhitespace) }
+        guard let number = T(text) else { return .failure(.notNumeric) }
+        guard number >= min && number <= max else { return .failure(.outOfRange(min: min, max: max)) }
+        
+        return .success(number)
+    }
+}
