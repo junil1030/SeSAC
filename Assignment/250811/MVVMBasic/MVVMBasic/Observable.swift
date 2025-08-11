67f68cf41a307f7460fa550dabbab5b26554e807
junil1030 <dccrdseo@naver.com>
2025-08-11 21:37:17 +0900

    feat: Observable 구현 중

---
+++ Assignment/250811/MVVMBasic/MVVMBasic/Observable.swift
@@ -0,0 +1,19 @@
+//
+//  Observable.swift
+//  MVVMBasic
+//
+//  Created by 서준일 on 8/11/25.
+//
+
+import Foundation
+
+class Observable<T> {
+    
+    private var value: T
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    
+}
