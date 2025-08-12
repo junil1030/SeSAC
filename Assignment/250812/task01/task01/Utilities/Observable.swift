394b0ba8c03765a8918b9f63b79c75e46143a7b1
junil1030 <dccrdseo@naver.com>
2025-08-12 15:49:31 +0900

    feat: Observable Class에서 bind, lazyBind 함수 구현

--- Assignment/250812/task01/task01/Utilities/Observable.swift
+++ Assignment/250812/task01/task01/Utilities/Observable.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+class Observable<T> {
+    
+    private var action: ((T) -> Void)?
+    
+    var value: T {
+        didSet {
+            action?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(action: @escaping (T) -> Void) {
+        action(value)
+        self.action = action
+    }
+    
+    func lazyBind(action: @escaping (T) -> Void) {
+        self.action = action
+    }
+}
