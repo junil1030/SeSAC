3ac32bfe435948ba9f90b0b091c0f0f8db6b92d4
junil1030 <dccrdseo@naver.com>
2025-08-13 15:16:00 +0900

    feat: Router패턴을 사용하여 case로 나누어 관리할 수 있도록 함

--- Assignment/250812/task01/task01/Network/ShoppingRouter.swift
+++ Assignment/250812/task01/task01/Network/ShoppingRouter.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+import Alamofire
+
+enum ShoppingRouter {
+    case searchKeyword(keyword: String, sort: SortType, start: Int, display: Int)
+    
+    var baseURL: String {
+        return "https://openapi.naver.com/v1/search/"
+    }
+    
+    var endPoint: URL {
+        switch self {
+        case .searchKeyword:
+            return URL(string: baseURL + "shop")!
+        }
+    }
+    
+    var method: HTTPMethod {
+        return .get
+    }
+    
+    var headers: HTTPHeaders {
+        switch self {
+        case .searchKeyword:
+            return  [
+                "X-Naver-Client-Id": Bundle.main.object(forInfoDictionaryKey: "X-Naver-Client-Id") as! String,
+                "X-Naver-Client-Secret": Bundle.main.object(forInfoDictionaryKey: "X-Naver-Client-Secret") as! String
+            ]
+        }
+    }
+    
+    var parameters: Parameters {
+        switch self {
+        case .searchKeyword(let keyword, let sort, let start, let display):
+            return [
+                "query": keyword,
+                "display": display,
+                "start": start,
+                "sort": sort.rawValue
+            ]
+        }
+    }
+}
