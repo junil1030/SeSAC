befa55e05909d2d2501b32a1df0bed3dfe458353
junil1030 <dccrdseo@naver.com>
2025-08-22 16:23:54 +0900

    feat: 프로젝트 생성 및 초기 설정
    - 프로젝트 생성
    - Codebase로 전환
    - ImageAsset, ColorSet 추가
    - 폴더 정리

---
+++ Assignment/250822/Tamagotchi/Tamagotchi/View/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tamagotchi
+//
+//  Created by 서준일 on 8/22/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+
+
+}
+
