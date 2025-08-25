ad396c0400a0851c51a780041b0b328298877885
junil1030 <dccrdseo@naver.com>
2025-08-25 21:07:24 +0900

    feat: CustomObservable 구현

---
+++ Assignment/250822/Tamagotchi/Tamagotchi/Sources/Utilities/CustomObservable.swift
@@ -0,0 +1,35 @@
+//
+//  CustomObservable.swift
+//  Tamagotchi
+//
+//  Created by 서준일 on 8/25/25.
+//
+
+import Foundation
+import Alamofire
+import RxSwift
+import RxCocoa
+
+final class CustomObservable {
+    
+    static func getLotto(query: String) -> Observable<Lotto> {
+        
+        return Observable<Lotto>.create { observer in
+            
+            let url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=\(query)"
+            
+            AF.request(url).responseDecodable(of: Lotto.self) { response in
+                switch response.result {
+                case .success(let value):
+                    observer.onNext(value)
+                    observer.onCompleted()
+                    
+                case .failure(let error):
+                    observer.onError(error)
+                }
+            }
+            
+            return Disposables.create()
+        }
+    }
+}
