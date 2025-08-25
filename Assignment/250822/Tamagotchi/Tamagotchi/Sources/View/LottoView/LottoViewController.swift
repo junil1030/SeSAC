5ec4e06442ad0a9588ee2873572ddf7bd72d308f
junil1030 <dccrdseo@naver.com>
2025-08-25 19:26:11 +0900

    feat: LottoView File, Dependency 추가
    - LottoViewController 생성
    - Alamofire 추가

---
+++ Assignment/250822/Tamagotchi/Tamagotchi/Sources/View/LottoView/LottoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LottoViewController.swift
+//  Tamagotchi
+//
+//  Created by 서준일 on 8/25/25.
+//
+
+import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
+
+final class LottoViewController: BaseViewController {
+    
+    private let tableView = UITableView()
+    private let searchBar = UISearchBar()
+    
+    private let disposeBag = DisposeBag()
+    
+    
+}
