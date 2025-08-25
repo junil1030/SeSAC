39e6ef08c9d0ca9e4d5a128778754b64ba5e020f
junil1030 <dccrdseo@naver.com>
2025-08-25 19:14:46 +0900

    feat: 0825 수업 내용 추가
    - Custom Observable 구현
    - flatMap을 사용하여 이중 Observable 형태 벗겨내기
    - RxSwift와 네트워크 통신 (성공 여부에 대해서만)

---
+++ Project/SeSAC7Week8RxBasic/SeSACRxThreads/Network/CustomObservable.swift
@@ -0,0 +1,72 @@
+//
+//  CustomObservable.swift
+//  SeSACRxThreads
+//
+//  Created by 서준일 on 8/25/25.
+//
+
+import Foundation
+import Alamofire
+import RxSwift
+
+struct Lotto: Decodable {
+    let drwNoDate: String
+    let firstAccumamnt: Int
+}
+
+final class CustomObservable {
+    
+    static func getLotto(query: String) -> Observable<Lotto> {
+        
+        return Observable<Lotto>.create { observer in
+            
+            let url = "https://www.dhlottery.co.kr/common.do?method=getLottoNumber&drwNo=\(query)"
+            
+            AF.request(url).responseDecodable(of: Lotto.self) { response in
+                switch response.result {
+                case .success(let value):
+                    print(value)
+                    observer.onNext(value)
+                    observer.onCompleted()
+                    
+                case .failure(let error):
+                    print(error)
+                    observer.onError(error)
+                }
+            }
+            
+            return Disposables.create()
+        }
+    }
+    
+    static func randomNumber() -> Observable<Int> {
+        
+        return Observable<Int>.create { observer in
+            
+            observer.onNext(Int.random(in: 1...100))
+            observer.onCompleted()
+            
+            return Disposables.create()
+        }
+    }
+    
+    static func recommendNickname() -> Observable<String> {
+        
+        return Observable<String>.create { observer in
+            let text: [String?] = ["고래밥", nil, "칙촉", nil, "카스타드", "몽쉘", "오예스"]
+            
+            guard let random = text.randomElement(), let result = random else {
+                observer.onError(junError.invalid)
+                return Disposables.create()
+            }
+            
+            if result == "카스타드" {
+                observer.onError(junError.invalid)
+            } else {
+                observer.onNext(result)
+            }
+            
+            return Disposables.create()
+        }
+    }
+}
---
+++ Project/SeSAC7Week8RxBasic/SeSACRxThreads/Network/Homework/HomeworkViewController.swift
@@ -0,0 +1,181 @@
+//
+//  HomeworkViewController.swift
+//  RxSwift
+//
+//  Created by Jack on 1/30/25.
+//
+
+import UIKit
+import Alamofire
+import SnapKit
+import RxSwift
+import RxCocoa
+
+class HomeworkViewController: UIViewController {
+    
+    let tableView = UITableView()
+    lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout())
+    let searchBar = UISearchBar()
+    
+    let disposeBag = DisposeBag()
+    
+    let list: BehaviorRelay<[Lotto]> = BehaviorRelay(value: [])
+    let items = BehaviorRelay(value: ["a", "b", "c"])
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        bind()
+    }
+    
+    private func bind() {
+        list
+            .bind(to: tableView.rx.items(cellIdentifier: PersonTableViewCell.identifier, cellType: PersonTableViewCell.self)) { row, element, cell in
+                let text = "\(element.drwNoDate)일, \(element.firstAccumamnt.formatted())원"
+                cell.usernameLabel.text = text
+            }
+            .disposed(by: disposeBag)
+        
+        items
+            .bind(to: collectionView.rx.items(cellIdentifier: UserCollectionViewCell.identifier, cellType: UserCollectionViewCell.self)) { row, element, cell in
+                cell.label.text = element
+            }
+            .disposed(by: disposeBag)
+        
+        tableView.rx.modelSelected(Int.self)
+            .map { "셀 \($0)" }
+            .bind(with: self) { owner, number in
+                var original = owner.items.value
+                original.insert(number, at: 0)
+                
+                owner.items.accept(original)
+            }
+            .disposed(by: disposeBag)
+        
+        searchBar.rx.searchButtonClicked
+            .withLatestFrom(searchBar.rx.text.orEmpty)
+            .distinctUntilChanged()
+            .flatMap { text in
+                CustomObservable.getLotto(query: text)
+                    .debug("커스텀 옵저버블 겟 로또")
+            }
+            .debug("서치바 옵저버블")
+            .subscribe(with: self) { owner, lotto in
+                print("onNext", lotto)
+                
+                var data = owner.list.value
+                data.insert(lotto, at: 0)
+                owner.list.accept(data)
+            } onError: { owner, error in
+                print("onError", error)
+            } onCompleted: { owner in
+                print("onCompleted")
+            } onDisposed: { owner in
+                print("onDisposed")
+            }
+            .disposed(by: disposeBag)
+
+    }
+    
+    func hide() {
+        // 커스텀 옵저버블을 활용해서, 서치 탭 시 랜덤으로 숫자를 추가
+        //        searchBar.rx.searchButtonClicked
+        //            .map {
+        //                return CustomObservable.randomNumber()
+        //            }
+        //            .bind(with: self) { owner, value in
+        //                value
+        //                    .bind(with: self) { owner, number in
+        //                        var data = owner.list.value
+        //                        data.insert(number, at: 0)
+        //
+        //                        owner.list.accept(data)
+        //                    }
+        //                    .disposed(by: owner.disposeBag)
+        //            }
+        //            .disposed(by: disposeBag)
+        
+        //        searchBar.rx.searchButtonClicked
+        //            .flatMap {
+        //                CustomObservable.randomNumber()
+        //                    .debug("랜덤넘버 옵저버블")
+        //            }
+        //            .debug("서치바 옵저버블")
+        //            .bind(with: self) { owner, number in
+        //                var data = owner.list.value
+        //                data.insert(number, at: 0)
+        //                owner.list.accept(data)
+        //            }
+        //            .disposed(by: disposeBag)
+        
+        //        searchBar.rx.searchButtonClicked
+        //            .withLatestFrom(searchBar.rx.text.orEmpty)
+        //            .map { Int($0) ?? 0 }
+        //            .bind(with: self) { owner, text in
+        //                var data = owner.list.value
+        //                data.insert(text, at: 0)
+        //                owner.list.accept(data)
+        //            }
+        //            .disposed(by: disposeBag)
+        
+        //        CustomObservable.randomNumber()
+        //            .bind(with: self) { owner, number in
+        //                owner.list.accept([number])
+        //            }
+        //            .disposed(by: disposeBag)
+        //
+        //        CustomObservable.recommendNickname()
+        //            .bind(with: self) { owner, nickname in
+        //                owner.items.accept([nickname])
+        //            }
+        //            .disposed(by: disposeBag)
+        //
+        //        CustomObservable.recommendNickname()
+        //            .subscribe(with: self) { owner, value in
+        //                owner.items.accept([value])
+        //                print("onNext", value)
+        //            } onError: { owner, error in
+        //                print("onError", error, owner)
+        //            } onCompleted: { owner in
+        //                print("OnComplete", owner)
+        //            } onDisposed: { owner in
+        //                print("OnDiposed", owner)
+        //            }
+        //            .disposed(by: disposeBag)
+    }
+    
+    private func configure() {
+        view.backgroundColor = .white
+        view.addSubview(tableView)
+        view.addSubview(collectionView)
+        view.addSubview(searchBar)
+        
+        navigationItem.titleView = searchBar
+         
+        collectionView.register(UserCollectionViewCell.self, forCellWithReuseIdentifier: UserCollectionViewCell.identifier)
+        collectionView.backgroundColor = .lightGray
+        collectionView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalToSuperview()
+            make.height.equalTo(50)
+        }
+        
+        tableView.register(PersonTableViewCell.self, forCellReuseIdentifier: PersonTableViewCell.identifier)
+        tableView.backgroundColor = .systemGreen
+        tableView.rowHeight = 100
+        tableView.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(50)
+            make.horizontalEdges.equalToSuperview()
+            make.bottom.equalToSuperview()
+        }
+    }
+    
+    private func layout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: 80, height: 40)
+        layout.scrollDirection = .horizontal
+        return layout
+    }
+
+}
+ 
