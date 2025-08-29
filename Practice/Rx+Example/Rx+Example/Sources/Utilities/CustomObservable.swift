c4c748fa497409a1264e950742c4cf502011130b
junil1030 <dccrdseo@naver.com>
2025-08-29 17:50:53 +0900

    feat: static function을 추가하여 Observable 형태로 받아오도록 구현

--- Practice/Rx+Example/Rx+Example/Sources/Utilities/CustomObservable.swift
+++ Practice/Rx+Example/Rx+Example/Sources/Utilities/CustomObservable.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+import Alamofire
+import RxSwift
+import RxCocoa
+
+final class CustomObservable {
+    
+    static func searchProduct(query: String, sort: SortType = .date, displayCount: Int = 30, start: Int) -> Observable<Result<ShoppingResponse, APIError>>  {
+        
+        return Observable.create { observer in
+            
+            NetworkManager.shared.fetchData(query: query, sort: sort, displayCount: displayCount, start: start) { response in
+                switch response {
+                case .success(let shoppingResponse):
+                    observer.onNext(.success(shoppingResponse))
+                case .failure(let error):
+                    observer.onNext(.failure(error))
+                }
+                
+                observer.onCompleted()
+            }
+            
+            return Disposables.create()
+        }
+    }
+}
