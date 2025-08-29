4d55329f1cdb58cbae04e4eba34ef003896809e7
junil1030 <dccrdseo@naver.com>
2025-08-29 15:20:29 +0900

    chore: project 설정 변경 및 씬 델리게이트 수정
    - info.plist의 파일 경로 변경
    - Scene Delegate에서 생성한 서치 뷰의 컨트롤러로 변경

--- Practice/Rx+Example/Rx+Example/Application/SceneDelegate.swift
+++ Practice/Rx+Example/Rx+Example/Application/SceneDelegate.swift
@@ -15,7 +15,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
-        let vc = UIViewController()
+        let vc = SearchViewController()
         let nav = UINavigationController(rootViewController: vc)
         
         window = UIWindow(windowScene: windowScene)
