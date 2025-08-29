6a1a5b5149b23f3cc6c77e59c3e29f6e1a4998f3
junil1030 <dccrdseo@naver.com>
2025-08-29 14:44:46 +0900

    feat: Base 생성
    - BaseView, ViewController 생성
    - ViewModel protocol 생성

---
+++ Practice/Rx+Example/Rx+Example/Sources/Base/BaseView.swift
@@ -0,0 +1,20 @@
+//
+//  BaseView.swift
+//  Rx+Example
+//
+//  Created by 서준일 on 8/29/25.
+//
+
+import UIKit
+
+class BaseView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ Practice/Rx+Example/Rx+Example/Sources/Base/BaseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  BaseViewController.swift
+//  Rx+Example
+//
+//  Created by 서준일 on 8/29/25.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
