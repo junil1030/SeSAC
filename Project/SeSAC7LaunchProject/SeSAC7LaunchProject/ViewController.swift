2194a78e8c8d847faa8c5157351e03ccdc9cef5d
junil1030 <dccrdseo@naver.com>
2025-09-24 09:49:45 +0900

    Init Project - LaunchProject

---
+++ Project/SeSAC7LaunchProject/SeSAC7LaunchProject/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SeSAC7LaunchProject
+//
+//  Created by 서준일 on 9/24/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var oneImageView: UIImageView!
+    @IBOutlet var twoImageView: UIImageView!
+    @IBOutlet var threeImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
