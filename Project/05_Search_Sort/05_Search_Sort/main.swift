d9a9fe638b88dacd00d222c9eea500582ec5de48
junil1030 <dccrdseo@naver.com>
2025-11-11 13:10:06 +0900

    feat: 1111 수업 내용 추가 - 선형 탐색과 이분 탐색 - 버블 정렬과 삽입 정렬

---
+++ Project/05_Search_Sort/05_Search_Sort/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  05_Search_Sort
+//
+//  Created by Bran on 4/8/25.
+//
+
+import Foundation
+
+
+var arr = [4, 1, 5, 9, 12, 99, 8]
+//mergeSort(&arr, 0, 7) // end = 마지막 인덱스 + 1
+bubbleSort(&arr)
+
+print(arr)
