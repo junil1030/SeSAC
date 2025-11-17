56bc3af3e47304781d7f1f8fb39d1365e990f4be
junil1030 <dccrdseo@naver.com>
2025-11-17 15:19:54 +0900

    feat: 1117 수업 추가 - animation 효과 추가

--- Project/SeSAC7SwiftUIArchitecture/SeSAC7SwiftUIArchitecture/MVI/AnimationView.swift
+++ Project/SeSAC7SwiftUIArchitecture/SeSAC7SwiftUIArchitecture/MVI/AnimationView.swift
@@ -7,9 +7,36 @@
 
 import SwiftUI
 
+struct CardModel: Hashable {
+    let color = Color.random()
+    let name: String
+    let number: Int
+}
+
+extension Color {
+    static func random() -> Color {
+        Color(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1)
+        )
+    }
+}
+
 struct AnimationView: View {
     
     @State private var isAnimated = false
+    @State private var isExpandable = false
+    @State private var currentCard = CardModel(name: "", number: 0)
+    @Namespace var animation
+    
+    var cardList = [
+        CardModel(name: "국민카드", number: 0),
+        CardModel(name: "신한카드", number: 1),
+        CardModel(name: "우리카드", number: 2),
+        CardModel(name: "농협카드", number: 3),
+        CardModel(name: "현대카드", number: 4),
+    ]
     
     var body: some View {
         VStack {
@@ -27,6 +54,13 @@ struct AnimationView: View {
                 }
             }
         }
+        .overlay {
+            if isAnimated {
+                DetailAnimationView(showDetail: $isAnimated,
+                                    animation: animation,
+                                    currentCard: currentCard)
+            }
+        }
     }
     
     func topTitle() -> some View {
@@ -34,7 +68,7 @@ struct AnimationView: View {
             .font(.largeTitle)
             .bold()
             .frame(maxWidth: .infinity,
-                   alignment: isAnimated ? .leading : .center)
+                   alignment: isExpandable ? .leading : .center)
             .overlay(alignment: .trailing) {
                 topOverlayButton()
             }
@@ -59,9 +93,63 @@ struct AnimationView: View {
     
     func cardView() -> some View {
         ScrollView {
-            ForEach(0..<5) { item in
+            ForEach(cardList, id: \.self) { item in
+                oneCard(item)
+            }
+        }
+        .overlay {
+            Rectangle()
+                .fill(.black.opacity(isExpandable ? 0 : 0.3))
+                .onTapGesture {
+                    withAnimation {
+                        isExpandable = true
+                    }
+                }
+        }
+    }
+    
+    func oneCard(_ card: CardModel) -> some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(card.color)
+            .frame(height: 150)
+            .frame(maxWidth: .infinity)
+            .padding(.horizontal)
+            .offset(y: CGFloat(card.number) * (isExpandable ? 0 : -100))
+            .matchedGeometryEffect(id: card, in: animation)
+            .onTapGesture {
+                currentCard = card
+                withAnimation {
+                    isAnimated = true
+                }
+            }
+    }
+}
+
+struct DetailAnimationView: View {
+    
+    @Binding var showDetail: Bool
+    
+    let animation: Namespace.ID
+    let currentCard: CardModel
+    
+    var body: some View {
+        ZStack {
+            Color.gray
+                .ignoresSafeArea()
+            VStack {
+                Text(currentCard.name)
                 RoundedRectangle(cornerRadius: 25)
+                    .fill(currentCard.color)
                     .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+                    .padding(.horizontal)
+                    .matchedTransitionSource(id: currentCard, in: animation)
+//                    .matchedGeometryEffect(id: currentCard, in: animation)
+                    .onTapGesture {
+                        withAnimation {
+                            showDetail = false
+                        }
+                    }
             }
         }
     }
